19f9a089f9c74496024f6afc6fed7a8a2640a7e2
schoolshark <129446927+schoolshark@users.noreply.github.com>
2025-11-12 22:50:03 +0100

    initial commit of external version 1.1.0
    This file contains a series of honeypot divs styled with Tailwind CSS classes for various colors and effects, intended for use with Vite.

---
+++ magic-window-honeypots.blade.php
@@ -0,0 +1,146 @@
+
+{{--
+
+    file: resources/views/components/magic-window-honeypots.blade.php
+    If you use Vite: Make a honeypot to ensure that Vite includes your tailwind-classes.
+    = batteries included
+
+    Activate or comment only the honeypots you need, even if the additional css-size is not big ...
+
+--}}
+
+<div id="honeypots" class="hidden">
+
+    {{-- slate --}}
+    <div class="bg-slate-50 dark:bg-slate-50 text-slate-950 shadow shadow-slate-600/40">shadow</div>
+    <div class="bg-slate-600/80 dark:bg-slate-600/80 text-slate-50 shadow-md shadow-slate-600/40">shadow-bigger</div>
+    <div class="rounded-lg group-focus:bg-slate-600/100 dark:group-focus:bg-slate-600/100"></div>
+    <div class="border-2 border-slate-600/80 dark:border-slate-600/80 focus:border-slate-600/100 dark:focus:border-slate-600/100"/>
+
+    {{-- gray --}}
+    <div class="bg-gray-50 dark:bg-gray-50 text-gray-950 shadow shadow-gray-600/40">shadow</div>
+    <div class="bg-gray-600/80 dark:bg-gray-600/80 text-gray-50 shadow-md shadow-gray-600/40">shadow-bigger</div>
+    <div class="rounded-lg group-focus:bg-gray-600/100 dark:group-focus:bg-gray-600/100"></div>
+    <div class="border-2 border-gray-600/80 dark:border-gray-600/80 focus:border-gray-600/100 dark:focus:border-gray-600/100"/>
+
+    {{-- zinc --}}
+    <div class="bg-zinc-50 dark:bg-zinc-50 text-zinc-950 shadow shadow-zinc-600/40">shadow</div>
+    <div class="bg-zinc-600/80 dark:bg-zinc-600/80 text-zinc-50 shadow-md shadow-zinc-600/40">shadow-bigger</div>
+    <div class="rounded-lg group-focus:bg-zinc-600/100 dark:group-focus:bg-zinc-600/100"></div>
+    <div class="border-2 border-zinc-600/80 dark:border-zinc-600/80 focus:border-zinc-600/100 dark:focus:border-zinc-600/100"/>
+
+    {{-- neutral --}}
+    <div class="bg-neutral-50 dark:bg-neutral-50 text-neutral-950 shadow shadow-neutral-600/40">shadow</div>
+    <div class="bg-neutral-600/80 dark:bg-neutral-600/80 text-neutral-50 shadow-md shadow-neutral-600/40">shadow-bigger</div>
+    <div class="rounded-lg group-focus:bg-neutral-600/100 dark:group-focus:bg-neutral-600/100"></div>
+    <div class="border-2 border-neutral-600/80 dark:border-neutral-600/80 focus:border-neutral-600/100 dark:focus:border-neutral-600/100"/>
+
+    {{-- stone --}}
+    <div class="bg-stone-50 dark:bg-stone-50 text-stone-950 shadow shadow-stone-600/40">shadow</div>
+    <div class="bg-stone-600/80 dark:bg-stone-600/80 text-stone-50 shadow-md shadow-stone-600/40">shadow-bigger</div>
+    <div class="rounded-lg group-focus:bg-stone-600/100 dark:group-focus:bg-stone-600/100"></div>
+    <div class="border-2 border-stone-600/80 dark:border-stone-600/80 focus:border-stone-600/100 dark:focus:border-stone-600/100"/>
+
+    {{-- red --}}
+    <div class="bg-red-50 dark:bg-red-50 text-red-950 shadow shadow-red-600/40">shadow</div>
+    <div class="bg-red-600/80 dark:bg-red-600/80 text-red-50 shadow-md shadow-red-600/40">shadow-bigger</div>
+    <div class="rounded-lg group-focus:bg-red-600/100 dark:group-focus:bg-red-600/100"></div>
+    <div class="border-2 border-red-600/80 dark:border-red-600/80 focus:border-red-600/100 dark:focus:border-red-600/100"/>
+
+    {{-- orange --}}
+    <div class="bg-orange-50 dark:bg-orange-50 text-orange-950 shadow shadow-orange-600/40">shadow</div>
+    <div class="bg-orange-600/80 dark:bg-orange-600/80 text-orange-50 shadow-md shadow-orange-600/40">shadow-bigger</div>
+    <div class="rounded-lg group-focus:bg-orange-600/100 dark:group-focus:bg-orange-600/100"></div>
+    <div class="border-2 border-orange-600/80 dark:border-orange-600/80 focus:border-orange-600/100 dark:focus:border-orange-600/100"/>
+
+    {{-- amber --}}
+    <div class="bg-amber-50 dark:bg-amber-50 text-amber-950 shadow shadow-amber-600/40">shadow</div>
+    <div class="bg-amber-600/80 dark:bg-amber-600/80 text-amber-50 shadow-md shadow-amber-600/40">shadow-bigger</div>
+    <div class="rounded-lg group-focus:bg-amber-600/100 dark:group-focus:bg-amber-600/100"></div>
+    <div class="border-2 border-amber-600/80 dark:border-amber-600/80 focus:border-amber-600/100 dark:focus:border-amber-600/100"/>
+
+    {{-- yellow --}}
+    <div class="bg-yellow-50 dark:bg-yellow-50 text-yellow-950 shadow shadow-yellow-600/40">shadow</div>
+    <div class="bg-yellow-600/80 dark:bg-yellow-600/80 text-yellow-50 shadow-md shadow-yellow-600/40">shadow-bigger</div>
+    <div class="rounded-lg group-focus:bg-yellow-600/100 dark:group-focus:bg-yellow-600/100"></div>
+    <div class="border-2 border-yellow-600/80 dark:border-yellow-600/80 focus:border-yellow-600/100 dark:focus:border-yellow-600/100"/>
+
+    {{-- lime --}}
+    <div class="bg-lime-50 dark:bg-lime-50 text-lime-950 shadow shadow-lime-600/40">shadow</div>
+    <div class="bg-lime-600/80 dark:bg-lime-600/80 text-lime-50 shadow-md shadow-lime-600/40">shadow-bigger</div>
+    <div class="rounded-lg group-focus:bg-lime-600/100 dark:group-focus:bg-lime-600/100"></div>
+    <div class="border-2 border-lime-600/80 dark:border-lime-600/80 focus:border-lime-600/100 dark:focus:border-lime-600/100"/>
+
+    {{-- green --}}
+    <div class="bg-green-50 dark:bg-green-50 text-green-950 shadow shadow-green-600/40">shadow</div>
+    <div class="bg-green-600/80 dark:bg-green-600/80 text-green-50 shadow-md shadow-green-600/40">shadow-bigger</div>
+    <div class="rounded-lg group-focus:bg-green-600/100 dark:group-focus:bg-green-600/100"></div>
+    <div class="border-2 border-green-600/80 dark:border-green-600/80 focus:border-green-600/100 dark:focus:border-green-600/100"/>
+
+    {{-- emerald --}}
+    <div class="bg-emerald-50 dark:bg-emerald-50 text-emerald-950 shadow shadow-emerald-600/40">shadow</div>
+    <div class="bg-emerald-600/80 dark:bg-emerald-600/80 text-emerald-50 shadow-md shadow-emerald-600/40">shadow-bigger</div>
+    <div class="rounded-lg group-focus:bg-emerald-600/100 dark:group-focus:bg-emerald-600/100"></div>
+    <div class="border-2 border-emerald-600/80 dark:border-emerald-600/80 focus:border-emerald-600/100 dark:focus:border-emerald-600/100"/>
+
+    {{-- teal --}}
+    <div class="bg-teal-50 dark:bg-teal-50 text-teal-950 shadow shadow-teal-600/40">shadow</div>
+    <div class="bg-teal-600/80 dark:bg-teal-600/80 text-teal-50 shadow-md shadow-teal-600/40">shadow-bigger</div>
+    <div class="rounded-lg group-focus:bg-teal-600/100 dark:group-focus:bg-teal-600/100"></div>
+    <div class="border-2 border-teal-600/80 dark:border-teal-600/80 focus:border-teal-600/100 dark:focus:border-teal-600/100"/>
+
+    {{-- cyan --}}
+    <div class="bg-cyan-50 dark:bg-cyan-50 text-cyan-950 shadow shadow-cyan-600/40">shadow</div>
+    <div class="bg-cyan-600/80 dark:bg-cyan-600/80 text-cyan-50 shadow-md shadow-cyan-600/40">shadow-bigger</div>
+    <div class="rounded-lg group-focus:bg-cyan-600/100 dark:group-focus:bg-cyan-600/100"></div>
+    <div class="border-2 border-cyan-600/80 dark:border-cyan-600/80 focus:border-cyan-600/100 dark:focus:border-cyan-600/100"/>
+
+    {{-- sky --}}
+    <div class="bg-sky-50 dark:bg-sky-50 text-sky-950 shadow shadow-sky-600/40">shadow</div>
+    <div class="bg-sky-600/80 dark:bg-sky-600/80 text-sky-50 shadow-md shadow-sky-600/40">shadow-bigger</div>
+    <div class="rounded-lg group-focus:bg-sky-600/100 dark:group-focus:bg-sky-600/100"></div>
+    <div class="border-2 border-sky-600/80 dark:border-sky-600/80 focus:border-sky-600/100 dark:focus:border-sky-600/100"/>
+
+    {{-- blue --}}
+    <div class="bg-blue-50 dark:bg-blue-50 text-blue-950 shadow shadow-blue-600/40">shadow</div>
+    <div class="bg-blue-600/80 dark:bg-blue-600/80 text-blue-50 shadow-md shadow-blue-600/40">shadow-bigger</div>
+    <div class="rounded-lg group-focus:bg-blue-600/100 dark:group-focus:bg-blue-600/100"></div>
+    <div class="border-2 border-blue-600/80 dark:border-blue-600/80 focus:border-blue-600/100 dark:focus:border-blue-600/100"/>
+
+    {{-- indigo --}}
+    <div class="bg-indigo-50 dark:bg-indigo-50 text-indigo-950 shadow shadow-indigo-600/40">shadow</div>
+    <div class="bg-indigo-600/80 dark:bg-indigo-600/80 text-indigo-50 shadow-md shadow-indigo-600/40">shadow-bigger</div>
+    <div class="rounded-lg group-focus:bg-indigo-600/100 dark:group-focus:bg-indigo-600/100"></div>
+    <div class="border-2 border-indigo-600/80 dark:border-indigo-600/80 focus:border-indigo-600/100 dark:focus:border-indigo-600/100"/>
+
+    {{-- violett --}}
+    <div class="bg-violet-50 dark:bg-violet-50 text-violet-950 shadow shadow-violet-600/40">shadow</div>
+    <div class="bg-violet-600/80 dark:bg-violet-600/80 text-violet-50 shadow-md shadow-violet-600/40">shadow-bigger</div>
+    <div class="rounded-lg group-focus:bg-violet-600/100 dark:group-focus:bg-violet-600/100"></div>
+    <div class="border-2 border-violet-600/80 dark:border-violet-600/80 focus:border-violet-600/100 dark:focus:border-violet-600/100"/>
+
+    {{-- purple --}}
+    <div class="bg-purple-50 dark:bg-purple-50 text-purple-950 shadow shadow-purple-600/40">shadow</div>
+    <div class="bg-purple-600/80 dark:bg-purple-600/80 text-purple-50 shadow-md shadow-purple-600/40">shadow-bigger</div>
+    <div class="rounded-lg group-focus:bg-purple-600/100 dark:group-focus:bg-purple-600/100"></div>
+    <div class="border-2 border-purple-600/80 dark:border-purple-600/80 focus:border-purple-600/100 dark:focus:border-purple-600/100"/>
+
+    {{-- fuchsia --}}
+    <div class="bg-fuchsia-50 dark:bg-fuchsia-50 text-fuchsia-950 shadow shadow-fuchsia-600/40">shadow</div>
+    <div class="bg-fuchsia-600/80 dark:bg-fuchsia-600/80 text-fuchsia-50 shadow-md shadow-fuchsia-600/40">shadow-bigger</div>
+    <div class="rounded-lg group-focus:bg-fuchsia-600/100 dark:group-focus:bg-fuchsia-600/100"></div>
+    <div class="border-2 border-fuchsia-600/80 dark:border-fuchsia-600/80 focus:border-fuchsia-600/100 dark:focus:border-fuchsia-600/100"/>
+
+    {{-- pink --}}
+    <div class="bg-pink-50 dark:bg-pink-50 text-pink-950 shadow shadow-pink-600/40">shadow</div>
+    <div class="bg-pink-600/80 dark:bg-pink-600/80 text-pink-50 shadow-md shadow-pink-600/40">shadow-bigger</div>
+    <div class="rounded-lg group-focus:bg-pink-600/100 dark:group-focus:bg-pink-600/100"></div>
+    <div class="border-2 border-pink-600/80 dark:border-pink-600/80 focus:border-pink-600/100 dark:focus:border-pink-600/100"/>
+
+    {{-- rose --}}
+    <div class="bg-rose-50 dark:bg-rose-50 text-rose-950 shadow shadow-rose-600/40">shadow</div>
+    <div class="bg-rose-600/80 dark:bg-rose-600/80 text-rose-50 shadow-md shadow-rose-600/40">shadow-bigger</div>
+    <div class="rounded-lg group-focus:bg-rose-600/100 dark:group-focus:bg-rose-600/100"></div>
+    <div class="border-2 border-rose-600/80 dark:border-rose-600/80 focus:border-rose-600/100 dark:focus:border-rose-600/100"/>
+
+</div>
